11b86a09d76ede18f5aa7a2129d551c8646adeb8
Nilesh <chettrinilesh25@gmail.com>
2025-12-02 22:09:01 +0530

    Refactor AppContext to use functional component and improve context value handling

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,16 +1,12 @@
-import { createContext } from "react";
-import App from "../App";
+/* eslint-disable react-refresh/only-export-components */
+import React, { createContext, useMemo } from 'react';
 
- export const AppContext = createContext();
+export const AppContext = createContext({});
 
+const AppContextProvider = ({ children }) => {
+  const value = useMemo(() => ({}), []);
+  console.log('AppContextProvider mounted');
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
 
-    export const AppProvider = (props) => {
-
-        const value = {};
-        return (
-
-            <AppContext.Provider value={{value}}>
-              {props.children}
-            </AppContext.Provider>
-        )
-    }
+export default AppContextProvider;
