974e6f7ba1afde80f38c124200c3cf02314411c6
Nilesh <chettrinilesh25@gmail.com>
2025-12-02 22:08:36 +0530

    Refactor import statements and formatting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { AppContextProvider } from './Context/AppContext.jsx'
-import {BrowserRouter} from "react-router-dom"; 
-createRoot(document.getElementById('root')).render(
- 
- 
- <BrowserRouter>
- <AppContextProvider>
-    <App />
-  </AppContextProvider  >
-  
-  </BrowserRouter>,
+import App from './App'
+import AppContextProvider from './Context/AppContext';
+
+import { BrowserRouter } from 'react-router-dom'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
 )
