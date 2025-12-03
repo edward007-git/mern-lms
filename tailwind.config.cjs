3367c8f1623399e33331b6c516587f9a638ac1fc
Nilesh <chettrinilesh25@gmail.com>
2025-12-03 20:08:11 +0530

    extended Tailwind theme with custom font sizes for headings and defaults

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+  
+    extend: {
+   fontSize: {
+    'course-details-heading-large': ['36px', '44px'],
+    'course-details-heading-small': ['26px', '36px'],
+    'home-heading-large': ['40px', '56px'],
+    'home-heading-small': ['28px', '34px'],
+    'default': ['16px', '24px']
+   },
+
+
+    },
   },
   plugins: [],
 }
