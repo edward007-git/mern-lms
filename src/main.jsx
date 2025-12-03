a6068c6e5191f13c7e10fd40ac6bd2e44af481e0
Nilesh <chettrinilesh25@gmail.com>
2025-12-03 19:46:56 +0530

    added ClerkProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import AppContextProvider from './Context/AppContext';
-
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 const root = createRoot(document.getElementById('root'))
 
 root.render(
   <BrowserRouter>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} 
+  afterSignOutUrl={'/'}>
     <AppContextProvider>
       <App />
     </AppContextProvider>
+    </ClerkProvider>
   </BrowserRouter>
 )
