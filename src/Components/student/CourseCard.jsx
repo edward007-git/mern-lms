894521db28ccfde0508ec04d799b6014de977a7a
Nilesh <chettrinilesh25@gmail.com>
2025-12-04 22:39:01 +0530

    added star icon image and fix coursecard and coursesection styling

--- src/Components/student/CourseCard.jsx
+++ src/Components/student/CourseCard.jsx
@@ -1,33 +1,39 @@
 import React, { useContext } from 'react'
 import {assets} from '../../assets/assets.js'
 import { AppContext } from '../../Context/AppContext'
+import { Link } from 'react-router-dom'
 
 const CourseCard = ({course}) => {
   const {currency}=useContext (AppContext)
   return (
-    <div>
-{/*<img src={course.courseThumbnail} alt='thumbnail.'/>*/}
-<div>
+   <Link
+  to={"/course/" + course._id}
+  onClick={() => scrollTo(0, 0)}
+  className="border border-gray-500/30 pb-6 overflow-hidden rounded-lg"
+>
 
-  <h3>{course.courseTitle}
+<img className='w-full' src={course.courseThumbnail} alt='thumbnail.'/>
+<div className='p-3 text-left'>
+
+  <h3 className='text-base font-semibold'>{course.courseTitle}
 
   </h3>
-  <p>{course.educator}</p>
-  <div>
+  <p className='text-gray-500'>{course.educator.name}</p>
+  <div className='flex items-center space-x-2'>
     <p>4.5</p>
-    <div>
+    <div className='flex'>
       {[...Array(5)].map((_,i)=>
-      (<img key={i} src={assets.star} alt='' />)
+      (<img key={i} src={assets.star} alt='' className='w-5 h-5'/>)
       
       )}
-      <p>22</p>
+      <p className='text-gray-500'>22</p>
     </div>
-    <p>{currency}{(course.coursePrice - course.discount * course.coursePrice /100).toFixed(2)}</p>
+    <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount * course.coursePrice /100).toFixed(2)}</p>
   </div>
 </div>
 
 
-    </div>
+    </Link>
   )
 }
 
