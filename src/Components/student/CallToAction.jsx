bd05fd50951b485a1f27fbdde061cc52ca344bc1
Nilesh <chettrinilesh25@gmail.com>
2025-12-06 21:21:55 +0530

    feat: add CallToAction component layout and content

--- src/Components/student/CallToAction.jsx
+++ src/Components/student/CallToAction.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-
 const CallToAction = () => {
   return (
     <div>
 
 
 
-    <h1>CallToAction</h1>
+    <h1>Learn anything,anytime,anywhere</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut exercitationem ex accusantium ab, inventore natus tenetur repudiandae id laboriosam a ipsum vitae doloribus odit adipisci deleniti? Perferendis enim iure a.</p>
+      <div>
+        <button className="group flex items-center gap-2 text-blue-600 font-medium">
+          Get Started
+      <span className="inline-block transform transition-all duration-300 group-hover:translate-x-0.5">
+           →
+    </span>
+</button>
+</div>
+
+
     </div>
   )
 }
